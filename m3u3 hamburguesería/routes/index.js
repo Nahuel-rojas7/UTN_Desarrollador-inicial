94b378083e1a0fa5650bd61ac7f42a324e125e2f
Nahuel Rojas <nahuel.rojas.u@gmail.com>
2024-11-19 20:41:55 -0300

    m3u4

--- m3u3 hamburguesería/routes/index.js
+++ m3u3 hamburguesería/routes/index.js
@@ -26,7 +26,7 @@ router.post('/',async(req, res, next) => {
     host: process.env.SMPT_HOST,
     port: process.env.SMPT_PORT,
     auth: {
-      user: process.env.MPT_USER,
+      user: process.env.SMPT_USER,
       pass: process.env.SMPT_PASS
     }
   });
